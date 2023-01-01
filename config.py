db239cd87c8716b96d4509def33254415c0fc805
Jegaa1 <121353390+Jegaa1@users.noreply.github.com>
2023-01-01 12:05:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ API_HASH = os.environ.get("API_HASH", "fcdc178451cd234e63faefd38895c991")
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "1965973276:AAFngpmzKFyESfcvQscv7S7Dmus7N-nksWI") 
 
-FORCE_SUB = os.environ.get("FORCE_SUB", "jasuranserials") 
+FORCE_SUB = os.environ.get("FORCE_SUB", "AsuranMoviefinder") 
 
 DB_NAME = os.environ.get("DB_NAME","asuranj")     
 
@@ -16,8 +16,8 @@ DB_URL = os.environ.get("DB_URL","mongodb+srv://video:video@cluster0.gp0rn.mongo
  
 FLOOD = int(os.environ.get("FLOOD", "10"))
 
-START_PIC = os.environ.get("START_PIC", "")
+START_PIC = os.environ.get("START_PIC", "https://ibb.co/pR8pQBH")
 
-ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '').split()]
+ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '88008764').split()]
 
 PORT = os.environ.get("PORT", "8080")
